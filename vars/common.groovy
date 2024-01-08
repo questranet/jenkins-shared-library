deef77890e1563a103ad20dbf3dfe2437651ac69
Wale <Adewale.megbabi@gmail.com>
2024-01-08 14:12:47 +0100

    Initial commit

--- vars/common.groovy
+++ vars/common.groovy
@@ -28,30 +28,30 @@ def codeCheckout() {
     }
 }
 
-//def appRelease(appType) {
-//  sh 'echo ${TAG_NAME} >VERSION'
-//  stage('Release App Version') {
-//    if (appType == "nodejs") {
-//      sh 'zip -r ${component}-${TAG_NAME}.zip *'
-//    }
-//    if (appType == "java") {
-//      sh 'mv target/${component}-1.0.jar ${component}.jar'
-//      sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION schema'
-//    }
-//    if (appType == "python") {
-//      sh 'zip -r ${component}-${TAG_NAME}.zip *'
-//    }
-//    if (appType == "nginx") {
-//      sh 'zip -r ${component}-${TAG_NAME}.zip *'
-//    }
-//    sh 'curl -sSf -u "admin:Admin123" -X PUT -T ${component}-${TAG_NAME}.zip "http://artifactory.waleapagun.online:8081/artifactory/${component}/${component}-${TAG_NAME}.zip"'
-//  }
-//}
-
 def appRelease(appType) {
-    stage('Docker Build') {
-        sh 'docker build -t 008089408493.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .'
-        sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 008089408493.dkr.ecr.us-east-1.amazonaws.com'
-        sh 'docker push 008089408493.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
+  sh 'echo ${TAG_NAME} >VERSION'
+  stage('Release App Version') {
+    if (appType == "nodejs") {
+      sh 'zip -r ${component}-${TAG_NAME}.zip *'
     }
+    if (appType == "java") {
+      sh 'mv target/${component}-1.0.jar ${component}.jar'
+      sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION schema'
+    }
+    if (appType == "python") {
+      sh 'zip -r ${component}-${TAG_NAME}.zip *'
+    }
+    if (appType == "nginx") {
+      sh 'zip -r ${component}-${TAG_NAME}.zip *'
+    }
+    sh 'curl -sSf -u "admin:Admin123" -X PUT -T ${component}-${TAG_NAME}.zip "http://artifactory.waleapagun.online:8081/artifactory/${component}/${component}-${TAG_NAME}.zip"'
+  }
 }
+
+//def appRelease(appType) {
+//    stage('Docker Build') {
+//        sh 'docker build -t 008089408493.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .'
+//        sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 008089408493.dkr.ecr.us-east-1.amazonaws.com'
+//        sh 'docker push 008089408493.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
+//    }
+//}
